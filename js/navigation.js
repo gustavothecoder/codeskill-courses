fcfdfa9e2c9aed762ecc23c4208e34c727d25950
Gustavo Ribeiro <g2_ribeiro@hotmail.com>
2020-03-28 20:09:12 -0300

    Adding functionality to the side menu buttons

---
+++ js/navigation.js
@@ -0,0 +1,34 @@
+function configureLinks() {
+    document.querySelectorAll('[link]')
+        .forEach(link => {
+            link.href = link.attributes['link'].value;
+        });
+}
+
+function navigate(hash) {
+    if (!hash) return
+
+    const link = document.querySelector(`[link='${hash}']`);
+    const destiny = document.querySelector('[destiny-link]');
+
+    destiny.innerHTML = link;
+    markButtonAsSelected(hash);
+}
+
+function markButtonAsSelected(hash) {
+    const links = document.querySelectorAll('[link]');
+    links.forEach(link => {
+        link.style.border = 'none';
+        link.style.padding = '10px 15px';
+        link.style.color = 'white';
+    })
+
+    const link = document.querySelector(`[link='${hash}']`);
+    if (link) {
+        link.style.border = '2px solid #00BFFF';
+        link.style.padding = '8px 13px'
+        link.style.color = '#00BFFF';
+    }
+}
+
+configureLinks();
